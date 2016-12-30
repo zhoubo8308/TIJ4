c7d8d7216aeb5800268f470a803522058a613877
zhoubo8308 <zhoubo8308@163.com>
2016-12-30 11:07:48 +0800

    make ignore work

--- concurrency/Joining.java
+++ concurrency/Joining.java
@@ -1,57 +1,54 @@
 //: concurrency/Joining.java
-package concurrency; /* Added by Eclipse.py */
+
 // Understanding join().
 import static net.mindview.util.Print.*;
 
 class Sleeper extends Thread {
-  private int duration;
-  public Sleeper(String name, int sleepTime) {
-    super(name);
-    duration = sleepTime;
-    start();
-  }
-  public void run() {
-    try {
-      sleep(duration);
-    } catch(InterruptedException e) {
-      print(getName() + " was interrupted. " +
-        "isInterrupted(): " + isInterrupted());
-      return;
-    }
-    print(getName() + " has awakened");
-  }
+	private int duration;
+
+	public Sleeper(String name, int sleepTime) {
+		super(name);
+		duration = sleepTime;
+		start();
+	}
+
+	public void run() {
+		try {
+			sleep(duration);
+		} catch (InterruptedException e) {
+			print(getName() + " was interrupted. " + "isInterrupted(): " + isInterrupted());
+			return;
+		}
+		print(getName() + " has awakened");
+	}
 }
 
 class Joiner extends Thread {
-  private Sleeper sleeper;
-  public Joiner(String name, Sleeper sleeper) {
-    super(name);
-    this.sleeper = sleeper;
-    start();
-  }
-  public void run() {
-   try {
-      sleeper.join();
-    } catch(InterruptedException e) {
-      print("Interrupted");
-    }
-    print(getName() + " join completed");
-  }
+	private Sleeper sleeper;
+
+	public Joiner(String name, Sleeper sleeper) {
+		super(name);
+		this.sleeper = sleeper;
+		start();
+	}
+
+	public void run() {
+		try {
+			sleeper.join();//被join的线程等join的线程执行完再执行，sleeper执行完再执行joiner。
+		} catch (InterruptedException e) {
+			print("Interrupted");
+		}
+		print(getName() + " join completed");
+	}
 }
 
 public class Joining {
-  public static void main(String[] args) {
-    Sleeper
-      sleepy = new Sleeper("Sleepy", 1500),
-      grumpy = new Sleeper("Grumpy", 1500);
-    Joiner
-      dopey = new Joiner("Dopey", sleepy),
-      doc = new Joiner("Doc", grumpy);
-    grumpy.interrupt();
-  }
-} /* Output:
-Grumpy was interrupted. isInterrupted(): false
-Doc join completed
-Sleepy has awakened
-Dopey join completed
-*///:~
+	public static void main(String[] args) {
+		Sleeper sleepy = new Sleeper("Sleepy", 5000), grumpy = new Sleeper("Grumpy", 5000);
+		Joiner dopey = new Joiner("Dopey", sleepy), doc = new Joiner("Doc", grumpy);
+		grumpy.interrupt();
+	}
+} /*
+	 * Output: Grumpy was interrupted. isInterrupted(): false Doc join completed
+	 * Sleepy has awakened Dopey join completed
+	 */// :~
